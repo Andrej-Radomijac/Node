a164581bd8fffee28a7491fcc9a2e3343370ed8e
krijesnica96 <1996role@gmail.com>
2017-12-21 20:52:33 +0100

    Update accountant.js

--- Quest/accountant.js
+++ Quest/accountant.js
@@ -26,6 +26,33 @@ var accountant = {
 	return {"jediPoints" : jediPoints,"sithPoints" : sithPoints};
 		
 	},
+	
+	"fCountPoints" : function(data){
+		
+	var questionHandler = require('./questionHandler.js');
+	var questions = questionHandler.fGetQuestions();
+	
+	var jediPoints = 0;
+	var sithPoints = 0;	
+		
+	jediPoints += questions[0].anwsers[data.fav_mov].jediPoints;
+	sithPoints += questions[0].anwsers[data.fav_mov].sithPoints;
+	
+	jediPoints += questions[1].anwsers[data.fav_jedi].jediPoints;
+	sithPoints += questions[1].anwsers[data.fav_jedi].sithPoints;
+	
+	jediPoints += questions[2].anwsers[data.fav_sith].jediPoints;
+	sithPoints += questions[2].anwsers[data.fav_sith].sithPoints;
+	
+	jediPoints += questions[3].anwsers[data.fav_planet].jediPoints;
+	sithPoints += questions[3].anwsers[data.fav_planet].sithPoints;
+	
+	jediPoints += questions[4].anwsers[data.force_select].jediPoints;
+	sithPoints += questions[4].anwsers[data.force_select].sithPoints;
+
+	return {"jediPoints" : jediPoints,"sithPoints" : sithPoints};
+	
+	},
 
 };
 
