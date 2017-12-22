17610f7b2de210a45ea8a878c3bffdf078eceeea
krijesnica96 <1996role@gmail.com>
2017-12-22 19:40:18 +0100

    Add files via upload

---
+++ Quest/repository/questionHandler.js
@@ -0,0 +1,84 @@
+var questions = [];
+var id = 0;
+
+var questionHandler = {
+
+	"fPostQuestions" : function(data){
+		
+		questions = data;
+		
+		var i = 0;
+		while(i<questions.length){
+			
+			questions[i++].id = id++;
+			
+		}
+		
+		
+	},
+	
+	"fFindQuestion" : function(data){
+		
+		var i = 0;
+		while(i<questions.length){
+			
+			if(data.id == questions[i].id){
+				
+				return questions[i]
+				
+			}
+			i++
+		}
+		return 11;
+		
+	},
+	
+	"fGetQuestions" : function() {
+		
+		if(questions.length!=0){
+			
+			return questions;
+			
+		}else{
+			
+			return 11;
+			
+		}
+		
+	},
+	
+	"fAddQuestion" : function(data){
+		
+		data.id = id++;
+		questions.push(data);
+		
+		
+	},
+	
+	"fDropQuestion" : function(data){
+		
+		
+		if(questions.length==0){
+			
+			return 11;
+			
+		}else{
+			
+			var i = 0;
+			while(i<questions.length){
+				if(data.question == questions[i].question){
+					
+					questions.splice(i,1);
+					return 0;
+				}
+				
+				i++;
+			}
+			
+		}
+		
+	},
+
+};
+
+module.exports = questionHandler;
