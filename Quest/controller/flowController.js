6095fc98b8eec7c5ed6309da121afd47bdf6b205
krijesnica96 <1996role@gmail.com>
2017-12-22 19:40:33 +0100

    Add files via upload

---
+++ Quest/controller/flowController.js
@@ -0,0 +1,58 @@
+var accountant = require('./service/accountant.js');
+var questionHandler = require('./questionHandler.js');
+
+var flowController = {
+	
+	"getQuestions" : function(){
+		
+		var flag = questionHandler.fGetQuestions();
+		
+		if(flag == 11){
+			
+			return "ERROR /GET QUESTIONS";
+			
+		}else{
+			console.log(flag);
+			return flag;
+			
+		}
+		
+	},
+	
+	"findQuestion" : function(data){
+		
+		var flag = questionHandler.fFindQuestion(data);
+		
+		return flag;
+		
+	},
+	
+	"postQuestions" : function(data){
+		
+		var flag = questionHandler.fPostQuestions(data);
+		
+	},
+	
+	"addQuestion" : function(data){
+		
+		var flag = questionHandler.fAddQuestion(data);
+		
+		
+	},
+
+	"dropQuestion" : function(data){
+		
+		
+		var flag = questionHandler.fDropQuestion(data);
+		
+		if(flag == 11){
+			
+			console.log('Nothing is deleted because list is empty!');
+			
+		}
+		
+	},
+
+};
+
+module.exports = flowController
