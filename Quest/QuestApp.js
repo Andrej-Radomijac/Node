a90f70879278290ec49dcf13bd1346c9ee9a7d3f
krijesnica96 <1996role@gmail.com>
2017-12-22 10:43:39 +0100

    Update QuestApp.js

--- Quest/QuestApp.js
+++ Quest/QuestApp.js
@@ -3,24 +3,12 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var bodyParser = require('body-parser');
-var session = require('express-session');
 
 var questions = require('./routes/questions');
 var index = require('./routes/index.js');
 
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "/views"));
-
-app.set('trust proxy', 1);
-
-app.use(session({
-	
-  cookieName: 'session',
-  secret: 'profQuest',
-  duration: 30 * 60 * 1000,
-  activeDuration: 5 * 60 * 1000,
-  
-}));
 
 app.use('/questions',questions);
 app.use('/',index);
