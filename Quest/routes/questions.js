c7251a9f325317659a32ab088574340d99f54116
krijesnica96 <1996role@gmail.com>
2017-12-27 20:30:57 +0100

    Update questions.js

--- Quest/routes/questions.js
+++ Quest/routes/questions.js
@@ -5,14 +5,13 @@ const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
 const accountant = require('../service/accountant');
 const flowController = require('../controller/flowController.js');
-var points;
 
 router.post('/resultsFromFile',jsonParser,function(req,res,next){
 	
 	const rawData = fs.readFileSync('./Backend.js');
 	const parsedData = JSON.parse(rawData);
 	
-	points = accountant.fCountPointsHardCode(parsedData,req.body);
+	const points = accountant.fCountPointsHardCode(parsedData,req.body);
 	
 	res.status(200).json(points);
 
@@ -20,16 +19,30 @@ router.post('/resultsFromFile',jsonParser,function(req,res,next){
 
 router.post('/postAnswers',jsonParser,function(req,res,next){
 	
-	points = accountant.fCountPoints(req.body);
-	res.status(200).json(points);
+	accountant.fCountPoints(req.body,function(err,result){
 	
+	if(err){
+		
+		res.status(500).json(err);
+		return;
+		
+	}
+	
+	res.status(200).json(result);
+	
+	});
 });
 
 router.post('/findQuestion',jsonParser,function(req,res,next){
 	
 	flowController.findQuestion(req.body,function(err,result){
 		
-		if(err) res.status(500).json(err);
+		if(err){
+			
+			res.status(500).json(err);
+			return;
+			
+		}
 		
 		res.status(200).json(result);
 		
@@ -37,17 +50,16 @@ router.post('/findQuestion',jsonParser,function(req,res,next){
 	
 });
 
-router.get('/getPoints',function(req,res,next){
-	
-	res.status(200).json(points);
-	
-});
-
 router.get('/getQuestions',function(req,res,next){
 	
 	flowController.getQuestions(function(err,result){
 	
-		if(err)res.status(500).json(err);
+		if(err){
+		
+			res.status(500).json(err);
+			return;
+			
+		}
 		
 		res.status(200).json(result);
 		
@@ -56,17 +68,29 @@ router.get('/getQuestions',function(req,res,next){
 
 router.post('/postQuestions',jsonParser,function(req,res,next){
 	
-	flowController.postQuestions(req.body);
+	flowController.postQuestions(req.body,function(err,result){
 	
-	res.status(200).json(questions);
+	if(err){
+		
+		res.status(500).json(err);
+		return;
+		
+	}
 	
+	res.status(200).json(result);
+	
+	});
 });
 
 router.post('/addQuestion',jsonParser,function(req,res,next){
 	
 	flowController.addQuestion(req.body,function(err,result){
 		
-		if(err)res.status(500).json(err);
+		if(err){
+			
+			res.status(500).json(err);
+			return;
+		}
 		
 		res.status(200).json(result);
 		
@@ -78,7 +102,12 @@ router.post('/dropQuestion',jsonParser,function(req,res,next){
 	
 	flowController.dropQuestion(req.body,function(err,result){
 	
-		if(err)res.status(500).json(err);
+		if(err){
+			
+			res.status(500).json(err);
+			return;
+		
+		}
 		
 		res.status(200).json(result);
 
