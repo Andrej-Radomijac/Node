7b512f4938a9d36d13f8a6dbf807705c5a8e35bb
krijesnica96 <1996role@gmail.com>
2017-12-27 16:48:23 +0100

    Update questionHandler.js

--- Quest/repository/questionHandler.js
+++ Quest/repository/questionHandler.js
@@ -1,7 +1,7 @@
 var questions = [];
 let id = 0;
 
-const questionHandler = {
+/*const questionHandler = {
 
 	"fPostQuestions" : function(data){
 		
@@ -14,21 +14,25 @@ const questionHandler = {
 			
 		}
 		
-		
+		return questions;
 	},
 	
 	"fFindQuestion" : function(data){
 		
+		if(questions.length == 0) return 12;
+		
 		let i = 0;
+		
 		while(i<questions.length){
 			
 			if(data.id == questions[i].id){
 				
-				return questions[i]
+				return questions[i];
 				
 			}
 			i++
 		}
+		
 		return 11;
 		
 	},
@@ -81,6 +85,95 @@ const questionHandler = {
 		
 	},
 
-};
+};*/
+class qHandler {
+	
+	qHandler(){
+		
+	};
+	
+	fPostQuestions(data){
+		
+		questions = data;
+		
+		let i = 0;
+		while(i<questions.length){
+			
+			questions[i++].id = id++;
+			
+		}
+		
+		return questions;
+	};
+	
+	fFindQuestion(data){
+		
+		if(questions.length == 0) return 12;
+		
+		let i = 0;
+		
+		while(i<questions.length){
+			
+			if(data.id == questions[i].id){
+				
+				return questions[i];
+				
+			}
+			i++
+		}
+		
+		return 11;
+		
+	};
+	
+	fGetQuestions() {
+		
+		if(questions.length!=0){
+			
+			return questions;
+			
+		}else{
+			
+			return 11;
+			
+		}
+		
+	};
+	
+	fAddQuestion(data){
+		
+		data.id = id++;
+		questions.push(data);
+		
+		return questions[questions.length-1];
+	};
+	
+	fDropQuestion(data){
+		
+		
+		if(questions.length==0){
+			
+			return 11;
+			
+		}else{
+			
+			let i = 0;
+			while(i<questions.length){
+				if(data.question == questions[i].question){
+					
+					questions.splice(i,1);
+					return questions;
+				}
+				
+				i++;
+			}
+			
+			return 11;
+		
+		}
+		
+	};
+}
 
-module.exports = questionHandler;
+module.exports = new qHandler();
+//module.exports = questionHandler;
