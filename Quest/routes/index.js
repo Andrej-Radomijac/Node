96d64d2c367c724ead194679ff46d02aa2d38142
krijesnica96 <1996role@gmail.com>
2018-01-15 18:59:28 +0100

    Update index.js

--- Quest/routes/index.js
+++ Quest/routes/index.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const router  = express.Router();
 const fs = require('fs');
-const bodyParser = require('body-parser');
-const jsonParser = bodyParser.json();
-const urlencodedParser = bodyParser.urlencoded({ extended: false })
 const accountant = require('../service/accountant');
 
 const rawData = fs.readFileSync('./Backend.js');
@@ -15,7 +12,7 @@ router.get('/',function(req,res,next){
 	
 });
 
-router.post('/',urlencodedParser,function(req,res,next){
+router.post('/',function(req,res,next){
 	
 	const points = accountant.fCountPointsHardCode(parsedData,req.body);
 	res.render('results',{jPoints : points.jediPoints, sPoints : points.sithPoints});
